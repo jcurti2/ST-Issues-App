2cdb6ba11ef7f25de60e3d1bf75a2d56633833d8
jcurti2 <joshua.curtis24@gmail.com>
2023-03-24 09:19:10 -0700

    post user models created, creating comment model

---
+++ migrations/20230324160444-create-post.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name:{
+        type: Sequelize.STRING
+      },
+      userId: {
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT('long')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts');
+  }
+};
---
+++ models/post.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Post.belongsTo(model.User, { foreignKey: 'userId'})
+      // define association here
+    }
+  }
+  Post.init({
+    name: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Post',
+    tableName: 'posts'
+  });
+  return Post;
+};
