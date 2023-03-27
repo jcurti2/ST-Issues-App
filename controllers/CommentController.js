6f61c3839f428075ef831264c149e8ac92fe4006
jcurti2 <joshua.curtis24@gmail.com>
2023-03-27 09:47:35 -0700

    routes and controllers for comments work

---
+++ controllers/CommentController.js
@@ -0,0 +1,32 @@
+const { Comment } = require('../models')
+
+const CreateComment = async (req, res) => {
+    try {
+        let userId = parseInt(req.params.userId)
+        let postId = parseInt(req.params.postId)
+        let commentBody = {
+            userId, postId,
+            ...req.body,
+        }
+        let comment = await Comment.create(commentBody)
+        res.send(comment)
+    } catch (error) {
+        throw error
+    }
+}
+
+const GetPostComments = async (req, res) => {
+    try {
+        const comments = await Comment.findAll({
+            where: { postId: req.params.postId }
+        })
+        res.send(comments)
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    CreateComment,
+    GetPostComments
+}
--- routes/App.Router.js
+++ routes/App.Router.js
@@ -2,11 +2,11 @@ const Router = require('express').Router()
 
 const UserRouter = require('./UserRouter')
 const PostRouter = require('./PostRouter')
-// const CommentRouter = require('./CommentRouter')
+const CommentRouter = require('./CommentRouter')
 
 Router.use('/user', UserRouter)
 Router.use('/post', PostRouter)
-// Router.use('/comment', CommentRouter)
+Router.use('/comment', CommentRouter)
 
 
 module.exports = Router
